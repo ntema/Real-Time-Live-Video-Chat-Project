645f75a0393bfbbc6dd972773a853c58d4798253
unknown <emmanuelntekim000@gmail.com>
2022-09-24 12:38:03 +0100

    added payment features

--- server.js
+++ server.js
@@ -3,9 +3,36 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
-
+const mongoose = require('mongoose')
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+
+const dbURI = 'mongodb://127.0.0.1:27017/testdb'
+ const conn = mongoose.connect(dbURI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(()=>{
+        console.log('database connected')
+    }).catch((err)=>{
+        console.log(err)
+    })
+    
+app.use(express.urlencoded({ limit: "1000000mb", extended: true }));
+app.use(express.json({ limit: "1000000mb", extended: true }));
+  //routes
+  //routes
+app.use('/api/auth', require('./Routes/auth/register'))
+app.use('/api/auth', require('./Routes/auth/login'))
+app.use('/api/user', require('./Routes/userRoutes/getAllUser'))
+app.use('/api/user', require('./Routes/userRoutes/getSingleUser'))
+app.use('/api/user', require('./Routes/userRoutes/updateUser'))
+app.use('/api/user', require('./Routes/userRoutes/deleteUser'))
+app.use('/api/transaction', require('./Routes/transaction/creditWallet'))
+app.use('/api/transaction', require('./Routes/transaction/withdraw'))
+app.use('/api/transaction', require('./Routes/transaction/transfer'))
+
 
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`)
@@ -14,6 +41,22 @@ app.get('/', (req, res) => {
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
 })
+
+app.use((req, res, next) => {
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error);
+  });
+  
+  app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+      error: {
+        status: error.status || 500,
+        message: error.message,
+      },
+    });
+  });
 
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
@@ -26,4 +69,7 @@ io.on('connection', socket => {
   })
 })
 
-server.listen(3000)
+ app.listen(3000, console.log('server running on port 3000'))
+
+
+
